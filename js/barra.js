91582d7bfb2995f9ee5798b901e103e3d3804007
Luis <luisbch9@gmail.com>
2021-07-22 12:19:05 -0300

    navbar responsive

--- js/barra.js
+++ js/barra.js
@@ -1,13 +1,9 @@
 window.addEventListener('scroll', function() {
-
     if ( window.scrollY > 100) {
-        // console.log("bajando");
-    
         document.getElementsByClassName("navbar")[0].classList.add("color-change");
-
     } else {
-        // console.log("llego arriba");
         document.getElementsByClassName("navbar")[0].classList.remove("color-change");
-    
     }
   });
+
+// Responsive Navbar
