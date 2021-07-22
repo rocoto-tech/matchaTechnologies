51ecff39a99817e67369edb6b153547485b612a2
Luis <luisbch9@gmail.com>
2021-07-22 20:43:30 -0300

    Menu navbar Responsive

--- js/barra.js
+++ js/barra.js
@@ -1,9 +1,25 @@
-window.addEventListener('scroll', function() {
-    if ( window.scrollY > 100) {
-        document.getElementsByClassName("navbar")[0].classList.add("color-change");
-    } else {
-        document.getElementsByClassName("navbar")[0].classList.remove("color-change");
-    }
-  });
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    document.getElementsByClassName("navbar")[0].classList.add("color-change");
+  } else {
+    document
+      .getElementsByClassName("navbar")[0]
+      .classList.remove("color-change");
+  }
+});
 
 // Responsive Navbar
+document.querySelector("#close").onclick = function () {
+  document
+    .querySelector("body > nav > div.navbar-desplegado")
+    .classList.add("oculto");
+  console.log("Click Close");
+};
+
+// Responsive Navbar
+document.querySelector("#menu").onclick = function () {
+  document
+    .querySelector("body > nav > div.navbar-desplegado")
+    .classList.remove("oculto");
+  console.log("Click abrir");
+};
